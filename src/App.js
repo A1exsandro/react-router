cfdd4c8c81b9cbee8a9921222e3cfe4125594e3a
A1exsandro <alexsandro.acp@gmail.com>
2022-07-21 15:52:33 -0400

    basic configuration of react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+
 import './App.css';
 
 function App() {
   return (
-    <h1>I am Alexsandro, Welcome!!!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>    
   );
 }
 
