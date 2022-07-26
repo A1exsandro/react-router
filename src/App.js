b6c68a20f57e3ad6670b19ba5b2d84421cc019eb
A1exsandro <alexsandro.acp@gmail.com>
2022-07-26 12:29:41 -0400

    add element presence

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
+import Presence from "./pages/Presence";
 import TicTacToe from "./pages/TicTacToe";
 
 import './App.css';
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path="presence" element={<Presence />} />
           <Route path="tictactoe" element={<TicTacToe />} />
         </Route>
       </Routes>
