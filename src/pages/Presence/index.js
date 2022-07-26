be415b0f5e0bbdc2f491fe9095e1ad519e5f8907
A1exsandro <alexsandro.acp@gmail.com>
2022-07-26 13:55:27 -0400

    add data static by useState

--- src/pages/Presence/index.js
+++ src/pages/Presence/index.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles.css';
 
 const Presence = () => {
+  const [cards, setCards] = useState([
+    {
+      name: 'Alex',
+      id: 1,
+      presence_less: 3,
+      presence_plus: 15
+    }
+  ]);
+
   return(
     <div className="container">
       <div className="card">
-        <div className="avatar">1</div>
+        <div className="avatar">{cards[0].id}</div>
         <div className="info">
           <div className="presence">
-            <span className="presence less">5 Faltas </span>
-            <span className="presence">25 Presenças</span>
+            <span className="presence less">{cards[0].presence_less} Faltas </span>
+            <span className="presence">{cards[0].presence_plus} Presenças</span>
           </div>
-          Alexsandro Antônio
+          {cards[0].name}
         </div>
         <div className="button">X</div>
       </div>
