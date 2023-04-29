34ce3c97ac92eb4a99ebd0a910fdc3cf2d3bdf36
A1exsandro <alexsandro.acp@gmail.com>
2023-04-29 14:10:00 -0400

    fix: changes to get database from home

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,80 +1,10 @@
-// import { pairsOfCards } from "../../Utils/cards_2";
-import { useEffect, useState } from "react";
-import { useMemory } from "../../contexts/MemoryContext"; 
-import Card from "../Card";
-import { CardListContainer } from "./styles";
-
-import { getStorage } from '../../services/Firebase'
-import { ref, getDownloadURL } from "firebase/storage" 
-import CardTwo from "../Card/CardTwo";
-
-const storage = getStorage() 
-
-const data = [
-  'butterfly',
-  'avocado',
-  'banana',
-  'bathroom',
-  'bedroom',
-  'breakfast',
-  'bridge',
-  'brothers', 
-  'closed',
-  'cook',
-  'dance',
-  'eat',
-  'fear',
-  'fire',
-  'film',
-  'fruits',
-  'kitchen',
-  'night',
-  'read',
-  'open',
-  'marketplace',
-  'play',
-  'smell',
-  'sleep',
-  'plant',
-  'tired'
-] 
-console.log('here in cards')
+import { useMemory } from "../../contexts/MemoryContext" 
+import Card from "../Card"
+import CardTwo from "../Card/CardTwo"
+import { CardListContainer } from "./styles"
 
 const CardList = () => { 
-  const { cards } = useMemory()
-  const [images, setImages] = useState([])
-  const [sounds, setSounds] = useState([])
-
-  // const image = [{}]
-
-  // console.log('make obj', image)
-
-  const promises = data.map((dt) => (
-    getDownloadURL(ref(storage, `images/${dt}.jpeg`))
-  ))
-  
-  const audioPromises = data.map((dt) => (
-    getDownloadURL(ref(storage, `audio/${dt}.mp3`))
-  ))
-
-  useEffect(() => {
-    Promise.all(promises)
-      .then((urls) => setImages(urls))
-
-    Promise.all(audioPromises)
-      .then((audios) => setSounds(audios))
-      // setCards(image)
-  },[])
-
-  for (let i = 0; i < data.length; i++) {
-    cards[i] = {
-      id: i + 1,
-      id2: (i + 1) * -1,
-      nameImg: data[i],
-      linkImg: images[i],
-      linkSound: sounds[i]
-    }
-  } 
+  const { cards } = useMemory() 
 
   return (
     <CardListContainer>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,73 @@
-import React from "react";
-import Webcam from "react-webcam";
+import React, { useEffect, useState } from "react"
+import Webcam from "react-webcam"
+
+import { getStorage } from '../../services/Firebase'
+import { ref, getDownloadURL } from "firebase/storage" 
+import { useMemory } from "../../contexts/MemoryContext"
+
+const storage = getStorage() 
+
+const data = [
+  'butterfly',
+  'avocado',
+  'banana',
+  'bathroom',
+  'bedroom',
+  'breakfast',
+  'bridge',
+  'brothers', 
+  'closed',
+  'cook',
+  'dance',
+  'eat',
+  'fear',
+  'fire',
+  'film',
+  'fruits',
+  'kitchen',
+  'night',
+  'read',
+  'open',
+  'marketplace',
+  'play',
+  'smell',
+  'sleep',
+  'plant',
+  'tired'
+] 
+console.log('here in cards')
 
 const Home = () => {
+  const { cards } = useMemory()
+  const [images, setImages] = useState([])
+  const [sounds, setSounds] = useState([])
+
+  const promises = data.map((dt) => (
+    getDownloadURL(ref(storage, `images/${dt}.jpeg`))
+  ))
+  
+  const audioPromises = data.map((dt) => (
+    getDownloadURL(ref(storage, `audio/${dt}.mp3`))
+  ))
+
+  useEffect(() => {
+    Promise.all(promises)
+      .then((urls) => setImages(urls))
+
+    Promise.all(audioPromises)
+      .then((audios) => setSounds(audios)) 
+  },[])
+
+  for (let i = 0; i < data.length; i++) {
+    cards[i] = {
+      id: i + 1,
+      id2: (i + 1) * -1,
+      nameImg: data[i],
+      linkImg: images[i],
+      linkSound: sounds[i]
+    }
+  } 
+
   return(
     <>
       <div>Home</div>
