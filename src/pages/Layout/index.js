2c12b6fb1021e463648274b4ace425edabc24a3b
A1exsandro <alexsandro.acp@gmail.com>
2023-04-29 13:41:58 -0400

    feat: add words and test context in layout

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
+import { useMemory } from "../../contexts/MemoryContext";
 
 const Layout = () => {
+  const { trying } = useMemory()
+  console.log(trying)
     return (
       <>
         <nav>
@@ -10,6 +13,8 @@ const Layout = () => {
             <div><Link to="/users">Users</Link></div>
             <div><Link to="/presence">Presence</Link></div>
             <div><Link to="/tictactoe">Jogo da Velha</Link></div>
+            <div><Link to="/words">Memory Game</Link></div>
+            <div>{trying}</div>
           </div>
         </nav>
   
