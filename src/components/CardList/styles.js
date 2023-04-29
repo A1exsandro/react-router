1f7576aefbeb50ebaba34469f6e84f115af07ee7
A1exsandro <alexsandro.acp@gmail.com>
2023-04-29 13:41:12 -0400

    feat: add cardList component

---
+++ src/components/CardList/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const CardListContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  padding-top: 2rem;
+  gap: 1rem;
+`
