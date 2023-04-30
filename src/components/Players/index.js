1407173ed7f0e9be201fc9e7ecb034c6b4ea29ff
A1exsandro <alexsandro.acp@gmail.com>
2023-04-30 17:52:39 -0400

    feat: add players components

---
+++ src/components/Players/index.js
@@ -0,0 +1,19 @@
+import { useMemory } from "../../contexts/MemoryContext"
+import { ContainerPlayers } from "./styles"
+
+const Players = () => {
+	const { players } = useMemory()
+	
+
+	return (
+		<ContainerPlayers>
+      {
+				players.map((player) => 
+					<div>{player.name} : {player.score}</div>
+				)
+			}
+    </ContainerPlayers>
+	)
+}
+
+export default Players
---
+++ src/components/Players/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const ContainerPlayers = styled.div`
+  display: flex; 
+  justify-content: space-around;
+  align-items: center;
+  background-color: black;
+  color: white;
+  width: 90%;
+  height: 2rem;
+  border-radius: 0 0 20px 20px;
+`
