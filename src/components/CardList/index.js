33749f3e757348c6f4ecfd1577d3220f8e566336
A1exsandro <alexsandro.acp@gmail.com>
2023-05-01 19:47:09 -0400

    feat: shuffle cards

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,20 +1,42 @@
+import React, { useEffect, useState } from "react"
 import { useMemory } from "../../contexts/MemoryContext" 
 import Card from "../Card"
-import CardTwo from "../Card/CardTwo"
+import CardTwo from "../Card/CardTwo"  
 import { CardListContainer } from "./styles"
 
 const CardList = () => { 
-  const { cards } = useMemory() 
+  const { cards, idFoundPairsCards, idFoundCards  } = useMemory() 
+  const [newCards, setNewCards] = useState([])
 
+  useEffect(() => {
+    const shuffleCards = (cards) => {
+      let currentIndex = cards.length;
+      let randomIndex;
+  
+      while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [cards[currentIndex], cards[randomIndex]] = [
+          cards[randomIndex],
+          cards[currentIndex],
+        ];
+      }
+  
+      return cards;
+    };
+  
+    setNewCards(shuffleCards(cards));
+  }, []);
+ 
   return (
-    <CardListContainer>
-      {cards.map((card) => (
+    <CardListContainer> 
+      {newCards.map((card) => (
         <Card key={card.id} {...card} /> 
       ))}
 
-      {cards.map((card) => ( 
+      {newCards.map((card) => ( 
           <CardTwo key={card.id2} {...card} />
-        ))}
+        ))} 
     </CardListContainer>
   );
 };
