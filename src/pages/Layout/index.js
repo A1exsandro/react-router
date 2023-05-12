84c8e3646ab49b2bd28bcb947143dd8f821775e4
A1exsandro <alexsandro.acp@gmail.com>
2023-05-12 12:37:07 -0400

    feat: add toggle menu

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,22 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import { Outlet, Link } from "react-router-dom"
 import { useMemory } from "../../contexts/MemoryContext"
 
 const Layout = () => {
   const { players, currentPlayerIndex } = useMemory() 
+  const [none, setNone] = useState(false)
+
+  const toggleMenu = () => {
+    setNone(!none)
+  }
   
     return (
       <>
         <nav>
-          <div><Link to="/">Home</Link></div>
+          <div onClick={toggleMenu}>Menu</div>
         </nav>
         
-        <div className="itemsMenu">
-          <div><Link to="/">Home</Link></div>
-          <div><Link to="/users">Users</Link></div>
-          <div><Link to="/presence">Presence</Link></div>
-          <div><Link to="/tictactoe">Jogo da Velha</Link></div>
-          <div><Link to="/words">Memory Game</Link></div>
+        <div className={`itemsMenu ${none ? "" : "none"}`}>
+          <div onClick={toggleMenu}><Link to="/">Home</Link></div>
+          <div onClick={toggleMenu}><Link to="/users">Users</Link></div>
+          <div onClick={toggleMenu}><Link to="/presence">Presence</Link></div>
+          <div onClick={toggleMenu}><Link to="/tictactoe">Jogo da Velha</Link></div>
+          <div onClick={toggleMenu}><Link to="/words">Memory Game</Link></div>
           {/* <div className="currentPlayers">{players[currentPlayerIndex].name} : {players[currentPlayerIndex].score}</div> */}
         </div>
   
