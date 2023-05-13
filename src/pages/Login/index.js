0c34e98ef57aa7da02441caf145cd26c8beb6c6e
A1exsandro <alexsandro.acp@gmail.com>
2023-05-13 14:07:47 -0400

    feat: login firebase with email and password

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,49 @@
-
+import React, { useState } from "react"
+import { useForm } from "react-hook-form"
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
 
 const Login = () => {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = login => {
+		setEmail(login.email)
+		setPassword(login.password) 
+	} 
+	
+	// LOGIN FIREBASE WITH EMAIL
+	const auth = getAuth()
+
+	createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code
+    const errorMessage = error.message
+    // ..
+  });
+
 	return (
-		<div>Login</div>
+		<>
+			<div>Login</div>
+      <div className="container">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>
+            E-mail
+            <input {...register("email")} />
+          </label>
+          <label>
+            Password
+            <input type="password"{...register("password")} />
+          </label>
+          <input type="submit" />
+        </form>
+      </div>
+    </>
 	)
 }
 
