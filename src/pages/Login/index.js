8c2cb2fec1aad88f4b63536799799c0fd814bcd2
A1exsandro <alexsandro.acp@gmail.com>
2023-05-13 16:15:58 -0400

    feat: signIn with google

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,21 +1,26 @@
 import React, { useState } from "react"
 import { useForm } from "react-hook-form"
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
+import {  
+	GoogleAuthProvider, 
+	signInWithPopup, 
+	createUserWithEmailAndPassword 
+} from "firebase/auth"
+import { auth } from "../../services/Firebase"
 
 const Login = () => {
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
 
-	const { register, handleSubmit, formState: { errors } } = useForm();
+	const { register, handleSubmit, formState: { errors } } = useForm()
   const onSubmit = login => {
 		setEmail(login.email)
 		setPassword(login.password) 
+		loginWithEmail()
 	} 
 	
-	// LOGIN FIREBASE WITH EMAIL
-	const auth = getAuth()
+	// LOGIN FIREBASE WITH EMAIL 
 
-	createUserWithEmailAndPassword(auth, email, password)
+	const loginWithEmail = () => createUserWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     // Signed in
     const user = userCredential.user
@@ -25,7 +30,33 @@ const Login = () => {
     const errorCode = error.code
     const errorMessage = error.message
     // ..
-  });
+  })
+
+	// LOGIN FIREBASE WITH GOOGLE
+
+	const provider = new GoogleAuthProvider() 
+	const loginWithGoogle = () => signInWithPopup(auth, provider) 
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+		console.log(token)
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+		console.log(user)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+		console.log(credential)
+    // ...
+  })
 
 	return (
 		<>
@@ -42,6 +73,7 @@ const Login = () => {
           </label>
           <input type="submit" />
         </form>
+				<button onClick={loginWithGoogle}>Login With Google</button>
       </div>
     </>
 	)
