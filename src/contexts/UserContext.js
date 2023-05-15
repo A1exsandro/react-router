abb072e9834b41f1411d88d95314cae35bf17244
A1exsandro <alexsandro.acp@gmail.com>
2023-05-15 11:30:24 -0400

    feat: changes in userContext

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,16 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from '../pages/Login'
 const { createContext, useContext, useState } = require("react")
 
 const UserContext = createContext({})
 
-export const UserContextProvider = (props) => {
-	const [infoUser, setInfoUser] = useState({})
+export const UserContextProvider = ({children}) => {
+	const [infoUser, setInfoUser] = useState(null)
+
+  const logout = () => {
+    // Lógica de logout para deslogar o usuário
+    setInfoUser(null);
+  };
+
+  const isAuthenticated = () => {
+    // Verifique se o usuário está autenticado
+    return infoUser !== null;
+  };
 
 	return(
 		<UserContext.Provider value={{ 
       infoUser,
-			setInfoUser
+			setInfoUser,
+      logout,
+      isAuthenticated,
     }}>
-      {props.children}
+      {children}
     </UserContext.Provider>
 	)
 }
@@ -18,3 +32,22 @@ export const UserContextProvider = (props) => {
 export const useUserContex = () => {
   return useContext(UserContext)
 }
+
+// Componente de guarda de rota
+// verificara a possibilidade de separar em um coponente
+export const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { isAuthenticated } = useContext(UserContext);
+
+  return ( 
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated() ? (
+          <Component {...props} />
+        ) : (
+          <Route path="/login" element={<Login />} />
+        )
+      }
+    /> 
+  );
+};
