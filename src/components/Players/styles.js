09e26f85d30ad8c6dc2db71445447d4b4f5c5dc6
A1exsandro <alexsandro.acp@gmail.com>
2023-06-07 09:12:47 -0400

    feat: media style

--- src/components/Players/styles.js
+++ src/components/Players/styles.js
@@ -14,11 +14,12 @@ export const ContainerPlayers = styled.div`
   font-weight: bold;
 
   @media only screen and (max-width: 1200px) {
-    font-size: 2rem;
+    font-size: 1rem;
   }
 
-  @media only screen and (max-width: 1200px) {
-    font-size: 1rem;
+  @media only screen and (max-width: 450px) {
+    font-size: .7rem;
+    height: 2.5rem;
   }
 `
 export const ShowPlayers = styled.div` 
