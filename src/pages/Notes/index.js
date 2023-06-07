647ab7daa03552848e4063bc231fa07f3c2f0563
A1exsandro <alexsandro.acp@gmail.com>
2023-06-07 10:53:52 -0400

    feat: add notes page

--- src/pages/Notes/index.js
+++ src/pages/Notes/index.js
@@ -1,12 +1,31 @@
-import React from "react" 
+import React, { useState } from "react" 
 
 const Notes = () => { 
+  const [notes, setNotes] = useState() 
+  const [valor, setValor] = useState()   
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    setValor(notes)
+  }
 
   return(
     <>
-      <div>Notes</div> 
+      <div>Notes alex</div> 
+
+      <form onSubmit={handleSubmit}>
+          <label>Enter your notes.......:
+            <input 
+              type="text"  
+              value={notes}
+              onChange={(e) => setNotes(e.target.value)}
+            />
+          </label> 
+          <input type="submit" />
+      </form> 
     </>
-  );
+  ) 
 }
 
-export default Notes;
+export default Notes 
