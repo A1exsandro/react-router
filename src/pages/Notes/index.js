89681c48f70be9d4e7bd3826adf45c8101e0ff42
A1exsandro <alexsandro.acp@gmail.com>
2023-06-07 12:16:51 -0400

    feat: add form and toggle show form

--- src/pages/Notes/index.js
+++ src/pages/Notes/index.js
@@ -2,28 +2,51 @@ import React, { useState } from "react"
 
 const Notes = () => { 
   const [notes, setNotes] = useState() 
-  const [valor, setValor] = useState()   
+  const [valor, setValor] = useState() 
+  const [showForm, setShowForm] = useState(false)
+  
+  const handleClick = () => {
+    setShowForm(true)
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault()
 
     setValor(notes)
+    setShowForm(false)
   }
+
+  const trocastyle = true
 
   return(
     <>
-      <div>Notes alex</div> 
-
-      <form onSubmit={handleSubmit}>
-          <label>Enter your notes.......:
+      <button
+        className="bg-black text-white p-2 rounded-md m-1"
+        onClick={handleClick}
+      >
+        Add Notes
+      </button>
+      
+      <div className="flex justify-center"> 
+        <form 
+          className={`${showForm ? 'flex flex-col' : 'hidden'} bg-slate-800 p-2 rounded-md text-white m-2`} 
+          onSubmit={handleSubmit}
+        >
+            <label>
+              <div className="text-center mb-2">Enter your notes</div>
+              <input 
+                className="rounded-md mb-2"
+                type="text"  
+                value={notes}
+                onChange={(e) => setNotes(e.target.value)}
+              />
+            </label> 
             <input 
-              type="text"  
-              value={notes}
-              onChange={(e) => setNotes(e.target.value)}
+              className="hover:bg-slate-700 cursor-pointer rounded-md"
+              type="submit" 
             />
-          </label> 
-          <input type="submit" />
-      </form> 
+        </form> 
+      </div>
     </>
   ) 
 }
